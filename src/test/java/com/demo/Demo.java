d8b0b4e6ae68f216a41bdf1b72a299cb39168ff2
michelin2210 <michelinsylvin2210@gmail.com>
2022-04-03 11:50:23 +0530

    Update Enumeration 3/4/2022 11:49 am

---
+++ src/test/java/com/demo/Demo.java
@@ -0,0 +1,25 @@
+package com.demo;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Demo {
+	private void methodEnumeration() {
+		Vector<Integer> v = new Vector<Integer>();
+		v.add(10);
+		v.add(20);
+		v.add(30);
+		v.add(80);
+		Enumeration<Integer> elements = v.elements();
+		while (elements.hasMoreElements()) {
+			Integer element = (Integer) elements.nextElement();
+			System.out.println(element);
+		}
+	}
+
+	public static void main(String[] args) {
+		Demo d = new Demo();
+		d.methodEnumeration();
+	}
+
+}
